3ecf5966f84f1ddc84de1717e40cb9898cb1e28c
tanmaykumarbala <tanmaykumarbala@gmail.com>
2021-06-22 20:17:43 +0530

    Changed icon in .conkyrc

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ copy="cp -rf .bash* conky .fehbg .startup terminator $configFolder/"
 copyVim="cp -rf .vimrc $HOME/.vimrc"
 copy1="cp -rf ./desktop.jpg $pictureFolder/desktop.jpg"
 copy2="cp -rf config btConnect.sh i3_startup $i3folder/"
+execWall="$HOME/.config/.fehbg"
 
 check_Folder_and_Install(){
 if [ -d "$i3folder" ] && [ -d "$pictureFolder" ]; then
@@ -18,6 +19,7 @@ if [ -d "$i3folder" ] && [ -d "$pictureFolder" ]; then
 		$($copy1)
 		$($copy2)
 		$($copyVim)
+		eval $execWall
 else
 		echo "Folder does not exist. Creating folders..."
 		eval mkdir -f $pictureFolder $i3folder
